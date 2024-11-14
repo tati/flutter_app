a3644cbbe6202a7c75265e631d91b7398c642d81
Tatiana J Tylosky <tatianajtylosky@Tatianas-MacBook-Pro.local>
2024-11-14 01:58:51 +0100

    changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,81 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
+import 'services/api_client.dart';
+
+class AstroPage extends StatefulWidget {
+  @override
+  _AstroPageState createState() => _AstroPageState();
+}
+
+class _AstroPageState extends State<AstroPage> {
+  final AstroApiClient _astroApiClient = AstroApiClient();
+  Map<String, dynamic>? sunTimes;
+  Map<String, dynamic>? moonTimes;
+
+  @override
+  void initState() {
+    super.initState();
+    fetchAstroData();
+  }
+
+  Future<void> fetchAstroData() async {
+    try {
+      final String location = 'newyork'; // Specify your location here
+      final DateTime date = DateTime.now();
+      final sunData = await _astroApiClient.getSunTimes(location, date);
+      final moonData = await _astroApiClient.getMoonTimes(location, date);
+
+      setState(() {
+        sunTimes = sunData;
+        moonTimes = moonData;
+      });
+    } catch (e) {
+      print('Error fetching astro data: $e');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Astronomical Data'),
+      ),
+      body: sunTimes == null || moonTimes == null
+          ? Center(child: CircularProgressIndicator())
+          : Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Sunrise: ${sunTimes?['results']['sunrise'] ?? 'N/A'}',
+                    style: TextStyle(fontSize: 16),
+                  ),
+                  Text(
+                    'Sunset: ${sunTimes?['results']['sunset'] ?? 'N/A'}',
+                    style: TextStyle(fontSize: 16),
+                  ),
+                  SizedBox(height: 20),
+                  Text(
+                    'Moonrise: ${moonTimes?['results']['moonrise'] ?? 'N/A'}',
+                    style: TextStyle(fontSize: 16),
+                  ),
+                  Text(
+                    'Moonset: ${moonTimes?['results']['moonset'] ?? 'N/A'}',
+                    style: TextStyle(fontSize: 16),
+                  ),
+                ],
+              ),
+            ),
+    );
+  }
+}
+
+
+
+
+
 
 void main() {
   runApp(MyApp());
@@ -37,8 +112,9 @@ class _MoonPhaseScreenState extends State<MoonPhaseScreen> {
 
   Future<void> fetchMoonPhaseData() async {
     final url = Uri.parse(
-        'https://api.nasa.gov/planetary/apod?api_key=pxLlMCt0Z8EpcYbTEcdTFSyhsn6sZFledUDEAfyF'); // Replace with the specific endpoint for moon data if available.
-       // 'https://api.nasa.gov/planetary/apod?api_key=$apiKey'); // Replace with the specific endpoint for moon data if available.
+        'https://api.xmltime.com/astronomy?api_key=M27xtssPLv'); // pull moon phase data
+       // 'https://api.nasa.gov/planetary/apod?api_key=pxLlMCt0Z8EpcYbTEcdTFSyhsn6sZFledUDEAfyF'); // Replace with the specific endpoint for moon data if available.
+    //https://dev.timeanddate.com/docs/astro/
     try {
       final response = await http.get(url);
       if (response.statusCode == 200) {
